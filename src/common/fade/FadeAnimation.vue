0ebc23b62b0d1c32e7196ca94534effe6f420f79
iamwumaixing <841240993@qq.com>
2019-07-13 15:42:29 +0800

    finish main

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,19 @@
+// 用于图片过渡动画效果
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Fade'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .v-enter, .v-leave-to
+    opacity 0
+  .v-enter-active, .v-leave-active
+    transition: opacity .5s
+</style>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="item" v-for="(item,index) of categoryList" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{ item.title }}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <!-- 使用递归 -->
+        <detail-list :categoryList="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 给组件一个名字主要原因是为了使用递归
+  name: 'DetailList',
+  props: {
+    categoryList: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height .8rem
+    font-size .32rem
+    padding 0 .2rem
+    .item-title-icon
+      position relative
+      left .06rem
+      top .06rem
+      display: inline-block;
+      width: .36rem;
+      height: .36rem;
+      background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+      margin-right: .1rem;
+      background-size: .4rem 3rem;
+  .item-children
+    padding 0 .2rem
+</style>
